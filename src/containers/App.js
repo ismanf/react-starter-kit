a7372f8ccb2a23f4bd85c2e1cbb55b6b45b3e7a7
Ismayil_Niftaliyev <ismayilturksoy@gmail.com>
2018-01-04 17:31:03 +0300

    refactored main component

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,5 +1,6 @@
 import React from 'react'
-import {Nav, NavItem, NavContainer} from '../components/common/Nav'
+import { Nav, NavItem, NavContainer } from '../components/common/Nav'
+import Footer from '../components/common/Footer'
 import Home from '../components/Home'
 import Service from '../components/Service'
 
@@ -22,6 +23,11 @@ const App = () => {
             <NavContainer>
                 <Nav items={dummy.navs} />
             </NavContainer>
+            <Footer>
+                <div className='container'>
+                    <p class='m-0 text-center text-white'>Copyright &copy; Ismayil Malik 2018</p>
+                </div>
+            </Footer>
         </div>
     )
 }
